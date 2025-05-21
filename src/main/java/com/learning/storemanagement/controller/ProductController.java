e47518a1da8d70e24144e12d8bb200e726c9303b
Maria Nistor-Brana <nistorbrana1@yahoo.com>
2025-05-21 14:08:48 +0300

    Add validation on product fields

--- src/main/java/com/learning/storemanagement/controller/ProductController.java
+++ src/main/java/com/learning/storemanagement/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.learning.storemanagement.controller;
 
 import com.learning.storemanagement.dto.ProductDTO;
 import com.learning.storemanagement.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO) {
+    public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO) {
         ProductDTO addedProduct = productService.addProduct(productDTO);
         return new ResponseEntity<>(addedProduct, HttpStatus.CREATED);
     }
--- src/main/java/com/learning/storemanagement/dto/ProductDTO.java
+++ src/main/java/com/learning/storemanagement/dto/ProductDTO.java
@@ -1,5 +1,6 @@
 package com.learning.storemanagement.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter
@@ -10,7 +11,12 @@ import lombok.*;
 public class ProductDTO {
 
     private Long id;
+    @NotEmpty(message = "Product name cannot be empty")
     private String name;
+    @Positive(message = "Product price must be greater than 0")
+    @NotNull(message = "Product price cannot be null")
     private Double price;
+    @NotNull(message = "Product stock cannot be null")
+    @Min(value = 0, message = "Product stock cannot be negative")
     private Integer stock;
 }
