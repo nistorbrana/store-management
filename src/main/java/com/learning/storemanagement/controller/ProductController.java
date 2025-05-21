168652ddc8cd69cfbb152e465270ff12af185ab1
Maria Nistor-Brana <nistorbrana1@yahoo.com>
2025-05-21 16:52:25 +0300

    Change controller to be rest

--- src/main/java/com/learning/storemanagement/controller/ProductController.java
+++ src/main/java/com/learning/storemanagement/controller/ProductController.java
@@ -6,12 +6,11 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/products")
 public class ProductController {
