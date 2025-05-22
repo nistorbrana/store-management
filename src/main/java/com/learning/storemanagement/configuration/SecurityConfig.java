00b2ca51544bb5cbefe64abff787068164ce7fd4
Maria Nistor-Brana <nistorbrana1@yahoo.com>
2025-05-22 18:31:40 +0300

    Encode user password and disable security

---
+++ src/main/java/com/learning/storemanagement/configuration/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.learning.storemanagement.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/learning/storemanagement/service/UserService.java
+++ src/main/java/com/learning/storemanagement/service/UserService.java
@@ -5,6 +5,7 @@ import com.learning.storemanagement.dto.builder.UserBuilder;
 import com.learning.storemanagement.model.StoreUser;
 import com.learning.storemanagement.repository.StoreUserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class UserService {
 
     private StoreUserRepository storeUserRepository;
     private UserBuilder storeUserBuilder;
+    private PasswordEncoder passwordEncoder;
 
     public List<UserDTO> findAll() {
         List<StoreUser> users = storeUserRepository.findAll();
@@ -24,8 +26,12 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
-    public UserDTO save(UserDTO storeUserDTO) {
-        StoreUser storeUser = storeUserBuilder.toEntity(storeUserDTO);
+    public UserDTO save(UserDTO userDTO) {
+        String encodedPassword = passwordEncoder.encode(userDTO.getPassword());
+
+        StoreUser storeUser = storeUserBuilder.toEntity(userDTO);
+        storeUser.setPassword(encodedPassword);
+
         return storeUserBuilder.toDTO(storeUserRepository.save(storeUser));
     }
 }
